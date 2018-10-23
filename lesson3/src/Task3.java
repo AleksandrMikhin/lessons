9b8f7eac61610f02b4f779faa2f589c49d1d4bc4
MikhinAlexander <mihanoidy@gmail.com>
2018-10-23 23:14:42 +0300

    домашнее 3

---
+++ lesson3/src/Task3.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int[] arr1 = new int[15];
+        int a = 0;
+        Random rnd = new Random(System.currentTimeMillis());
+
+        for (int i = 0; i<15; i++){
+            arr1[i] = rnd.nextInt(9);
+            System.out.print(arr1[i] + " ");
+            if (arr1[i]%2 == 0) {
+                a++;
+            }
+        }
+        System.out.println("\nЧетных элементов: " + a);
+    }
+}
---
+++ lesson3/src/TaskString2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class TaskString2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите строку: ");
+        StringBuilder strBuilder = new StringBuilder();
+        strBuilder.append(in.nextLine());
+
+        int i = 0, j;
+        while (i < strBuilder.length()) {
+            j = i + 1;
+            while (j < strBuilder.length()) {
+                if (strBuilder.charAt(i) == strBuilder.charAt(j) || strBuilder.charAt(j) == ' '){
+                    strBuilder.deleteCharAt(j);
+                }else{
+                    j++;
+                }
+            }
+            i++;
+        }
+        System.out.println(strBuilder.toString());
+    }
+}
